ba5912bf2db78cecaf78197011c0a3608a7eb320
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2023-06-30 20:01:30 -0300

    MyBudget | Estrutura basica do projeto

---
+++ myindex.py
@@ -0,0 +1,44 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from app import *
+from components import sidebar, dashboards, extratos
+
+
+
+
+# =========  Layout  =========== #
+content = html.Div(id="page-content")
+
+
+app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ], md=2, style={'background-color': 'red', 'height': '1080px'}),
+        dbc.Col([
+            content
+        ], md=10, style={'background-color': 'blue', 'height': '1080px'})        
+    ])
+    
+
+], fluid=True,)
+
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
+    
+    if pathname == '/extratos':
+        return extratos.layout
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(port=8051, debug=True)
