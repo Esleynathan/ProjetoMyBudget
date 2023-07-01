1d5c9602cb3a133de85799d55d45c7ac3ea3e533
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2023-07-01 11:13:25 -0300

    MyBudget | Criando o modal

--- components/sidebar.py
+++ components/sidebar.py
@@ -39,6 +39,23 @@ layout = dbc.Col([
                                 children=['- Despesa'])
                     ], width=6)
                 ]),
+
+                # Modal Receita
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar receita')),
+                    dbc.ModalBody([
+
+                    ])
+                ], id='modal-novo-receita'),
+
+                # Modal Despesa
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar despesa')),
+                    dbc.ModalBody([
+
+                    ])
+                ], id='modal-novo-despesa'),
+
     # Seção NAV --------------- 
 
                 html.Hr(),
@@ -53,3 +70,21 @@ layout = dbc.Col([
 
 # =========  Callbacks  =========== #
 # Pop-up receita
+@app.callback(
+    Output('modal-novo-receita', 'is_open'),
+    Input('open-novo-receita', 'n_clicks'),
+    State('modal-novo-receita', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
+    
+# Pop-up despesa
+@app.callback(
+    Output('modal-novo-despesa', 'is_open'),
+    Input('open-novo-despesa', 'n_clicks'),
+    State('modal-novo-despesa', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
